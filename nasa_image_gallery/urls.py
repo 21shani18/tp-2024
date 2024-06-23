9e47b186e99def07f91f97f3b446c5ecad1a429a
21shani18 <shanirapaz20@gmail.com>
2024-06-23 18:47:59 -0300

    login funcionando re pro

--- nasa_image_gallery/urls.py
+++ nasa_image_gallery/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.urls import include
 
 urlpatterns = [
     path('', views.index_page, name='index-page'),
     path('login/', views.index_page, name='login'),
     path('home/', views.home, name='home'),
     path('buscar/', views.search, name='buscar'),
+    path('accounts/', include ('django.contrib.auth.urls')),
 
     path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
     path('favourites/add/', views.saveFavourite, name='agregar-favorito'),
